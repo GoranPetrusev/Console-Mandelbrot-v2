55d4f43c4693086a0050e6870e1f4b9b37235bba
GoranPetrusev <46924437+GoranPetrusev@users.noreply.github.com>
2021-03-28 12:11:19 +0200

    more comments

--- Console Mandelbrot v2/Console Mandelbrot v2.cpp
+++ Console Mandelbrot v2/Console Mandelbrot v2.cpp
@@ -14,7 +14,7 @@ int main()
 	SetConsoleActiveScreenBuffer( hConsole );
 	DWORD dwBytesWritten = 0; // Idk man, windows moment
 
-	// Declaring the variables outside of the loops so they don't have to be created like a trillion times
+	// Declaring the variables outside of the loops so they don't get created like a trillion times
 	double x0,y0;
 	double x,y;
 	int iteration;
@@ -37,17 +37,17 @@ int main()
 		scaling *= (GetAsyncKeyState( 0x53 ))	? 1.03 : 1; // S - Scaling Down
 		scaling /= (GetAsyncKeyState( 0x57 ))	? 1.03 : 1; // W - Scaling Up
 
-		x_off += GetAsyncKeyState( VK_LEFT )	? 3 * fElapsedTime : 0;
-		x_off -= GetAsyncKeyState( VK_RIGHT )	? 3 * fElapsedTime : 0;
-		y_off += GetAsyncKeyState( VK_UP )	? 3 * fElapsedTime : 0;
-		y_off -= GetAsyncKeyState( VK_DOWN )	? 3 * fElapsedTime : 0;
+		x_off += GetAsyncKeyState( VK_LEFT )	? 3 * fElapsedTime : 0; // Move Left
+		x_off -= GetAsyncKeyState( VK_RIGHT )	? 3 * fElapsedTime : 0; // Move Right
+		y_off += GetAsyncKeyState( VK_UP )	? 3 * fElapsedTime : 0; // Move Up
+		y_off -= GetAsyncKeyState( VK_DOWN )	? 3 * fElapsedTime : 0; // Move Down
 
 
 		// Using the chrono to capture how long it takes a to render a particular frame
 		// The elapsed time is used to smooth out the panning
 		start = std::chrono::steady_clock::now();
 
-		// Mandelbrot calculation starts here
+		// Mandelbrot calculation is done in the scope of this for() lopp
 		for( int py = 0; py < n_screenHeight; py++ )
 		{
 			y0 = ((double)py * 0.066666666667 - y_off) * scaling;
